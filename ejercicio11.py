1ed1baf6b300d1dde8198ad6f266979f21586df8
COMPU <jessicatituana.vvc.1a@gmail.com>
2022-04-02 21:25:05 -0500

    commit

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,2 +1,15 @@
 """EJERCICIO 11.-Escribir un programa que lea un año indicar si es bisiesto. Nota: un año es bisiesto si es un número 
 divisible por 4, pero no si es divisible por 100, excepto que también sea divisible por 400."""
+
+año = int(input('Introducir un año: '))
+
+if año % 4 == 0:
+    if año % 100 == 0:
+        if año % 400 == 0:
+            print('El año es bisiesto')
+        else:
+            print('El año no es bisiesto')
+    else:
+        print('El año es bisiesto.')
+else:
+    print('El año no es bisiesto.')
