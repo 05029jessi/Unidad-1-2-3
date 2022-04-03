8c3869da50f3ad986b54857a795970e3fc7ba066
COMPU <jessicatituana.vvc.1a@gmail.com>
2022-04-02 22:55:28 -0500

    comit

--- ejercicio14.py
+++ ejercicio14.py
@@ -1,3 +1,15 @@
 """EJERCICIO 14.- Crea una aplicación que pida un número y calcule su factorial 
 (El factorial de un número es el producto de todos los enteros entre 1 y el propio número y
- se representa por el número seguido de un signo de exclamación. Por ejemplo 5! = 1x2x3x4x5=120"""
+se representa por el número seguido de un signo de exclamación. Por ejemplo 5! = 1x2x3x4x5=120"""
+
+numero = int(input("Ingresar un número: "))
+
+if numero !=0:
+    fact = 1
+    for i in range(1,numero+1):      #range(inicio de i, fin de i (se debe añadir un número más)   )
+        fact= i*fact
+    print(f"El factorial de", numero,"! es:",fact)
+
+else:
+    print("Ha ingresado 0, por favor ingrese un valor positivo")
+
--- ejercicio15.py
+++ ejercicio15.py
@@ -1,3 +1,16 @@
 """EJERCICIO 15.- Crea una aplicación que permita adivinar un número. En primer lugar la aplicación solicita un número 
 entero por teclado. A continuación va pidiendo números y va respondiendo si el número a adivinar es mayor o menor 
 que el introducido. El programa termina cuando se acierta el número."""
+
+adivinar=int(input("Ingresar el número a adivinar: "))
+numero=int(input("Ingresar un número: "))
+
+while numero!=adivinar:
+    if numero>adivinar:
+        print("El número a adivinar es menor que el introducido")
+        numero=int(input("Ingresar un número: "))
+    else:
+        print("El número a adivinar es mayor que el introducido")
+        numero=int(input("Ingresar un número: "))
+
+print("Número correcto")
